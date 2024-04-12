924ecbda7f75b53fa7fb6a962558659d0bebdaa5
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-12 17:49:51 +0530

    Update App.js
    Changed url address

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://www.google.com/search?q=site%3Arobinhood.com+${randSearch}&sca_esv=88aa776b28e671aa&sca_upv=1&ei=1-sXZszVMPjQseMPxMyB0A4&ved=0ahUKEwiMnbaTqLqFAxV4aGwGHURmAOoQ4dUDCBA&uact=5&oq=site%3Arobinhood.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFHNpdGU6cm9iaW5ob29kLmNvbSBhSIrBB1CnwAdYp8AHcAN4AJABAJgBAKABAKoBALgBA8gBAPgBAZgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
+    window.location = `https://www.google.com/search?q=site%3Aeatthis.com+${randSearch}&sca_esv=12fe31487ad231eb&sca_upv=1&ei=GSYZZpnDJqHh4-EP0uuSqAc&ved=0ahUKEwjZs_Ls07yFAxWh8DgGHdK1BHUQ4dUDCBA&uact=5&oq=site%3Aeatthis.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiEnNpdGU6ZWF0dGhpcy5jb20gYUi1vQhQAFgAcAJ4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
   }
   return (
     <div className='App' onClick={handleClick}>
