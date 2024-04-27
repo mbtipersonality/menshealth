9d82a143b27591d8b4604f6a7aba346dc753d7d6
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-27 09:22:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
     
-    window.location = `https://www.google.com/search?q=site%3Amenshealth.com+${randSearch}&sca_esv=44f1fb0a97cf7ff0&sca_upv=1&ei=a88bZqmBOJGE4-EPgYKxiA4&ved=0ahUKEwjp4oHO3cGFAxURwjgGHQFBDOEQ4dUDCBA&uact=5&oq=site%3Amenshealth.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWVuc2hlYWx0aC5jb20gYUiqjAhQAFgAcAR4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
+    window.location = `https://www.google.com/search?q=site%3Amenshealth.com/sex-women+${randSearch}&sca_esv=44f1fb0a97cf7ff0&sca_upv=1&ei=a88bZqmBOJGE4-EPgYKxiA4&ved=0ahUKEwjp4oHO3cGFAxURwjgGHQFBDOEQ4dUDCBA&uact=5&oq=site%3Amenshealth.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWVuc2hlYWx0aC5jb20gYUiqjAhQAFgAcAR4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
   }
   return (
     <div className='App' onClick={handleClick}>
