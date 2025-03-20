aa395ba64d8f2cf3ab51cabe31a69e604e5c8bcc
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-03-20 20:36:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const randSearch = search[Math.floor(Math.random() * (1000 - 1 + 1) + 1 )]
     
     // window.location = `https://www.google.com/search?q=site%3Amenshealth.com/sex-women+${randSearch}&sca_esv=44f1fb0a97cf7ff0&sca_upv=1&ei=a88bZqmBOJGE4-EPgYKxiA4&ved=0ahUKEwjp4oHO3cGFAxURwjgGHQFBDOEQ4dUDCBA&uact=5&oq=site%3Amenshealth.com+a&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXNpdGU6bWVuc2hlYWx0aC5jb20gYUiqjAhQAFgAcAR4AJABAJgBAKABAKoBALgBA8gBAJgCAKACAJgDAIgGAZIHAKAHAA&sclient=gws-wiz-serp`
-    window.location = `https://redditfavorites.com/?q=${randSearch}`
+    window.location = `https://yewtu.be/search?q=${randSearch}`
     // window.location = `https://redditfavorites.com/r/${randSearch}`
   }
   return (
